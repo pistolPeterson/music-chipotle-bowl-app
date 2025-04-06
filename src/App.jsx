8832ab2498f66fc75522672748c16742a7f713b7
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2025-04-06 17:52:47 -0400

    creating header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
-import { Button } from '@salt-ds/core';
+import { Button, Divider } from '@salt-ds/core';
 import { MealTitle } from './features/meal-title/MealTitle.jsx';
 function App() {
   return (
     <>
       <MealTitle exampleProp="hi?"></MealTitle>
+      <Divider></Divider>
       <div className="card">
         <Button sentiment="accented" appearance="solid" loading="true">
           Button
--- src/features/meal-title/MealTitle.jsx
+++ src/features/meal-title/MealTitle.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './mealTitle.css';
 import image1 from '../../assets/music chipotle bowl.png';
+import { Display1, Display2, Display3, Divider } from '@salt-ds/core';
 
 export const MealTitle = ({ exampleProp = 'Default Value' }) => {
   // const [state, setState] = useState(null);
@@ -8,11 +9,23 @@ export const MealTitle = ({ exampleProp = 'Default Value' }) => {
   //Use salt ui text field
   return (
     <div className="mealTitle">
-      <div className="mealTitle-img">
-        Image
+      <div className="left-cont">
         <img className="meal-img" src={image1} />
       </div>
-      <div className="mealTitle-text-section">Text Section</div>
+      <div className="right-cont">
+        <Display2>Build Your</Display2>
+        <br></br>
+        <Display1>
+          {' '}
+          <strong>Music Bowl </strong>{' '}
+        </Display1>
+        <br></br>
+        <br></br>
+        <Display3>
+          Select your instruments, moods and genres and get your custom made
+          music references!
+        </Display3>
+      </div>
     </div>
   );
 };
